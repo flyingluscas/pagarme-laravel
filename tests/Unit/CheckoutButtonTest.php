38f700d718fdb8521ccd71d741671c5ab1e38740
Lucas Pires <lucas.pires.mattos@gmail.com>
2017-05-26 17:52:47 -0300

    checkout button test refactored

--- tests/Unit/CheckoutButtonTest.php
+++ tests/Unit/CheckoutButtonTest.php
@@ -4,38 +4,49 @@ namespace FlyingLuscas\PagarMeLaravel;
 
 class CheckoutButtonTest extends UnitTestCase
 {
+    /**
+     * @var \FlyingLuscas\PagarMeLaravel\CheckoutButton
+     */
+    protected $button;
+
+    public function setUp()
+    {
+        $this->button = new CheckoutButton;
+    }
+
     public function testIfCanRenderScriptTag()
     {
         $this->assertEquals(
             '<script type="text/javascript" src="https://assets.pagar.me/checkout/checkout.js"></script>',
-            (new CheckoutButton)->render()
+            $this->button->render()
         );
     }
 
     public function testIfCanSetAttribute()
     {
-        $button = (new CheckoutButton)
-            ->setAttribute('data-amount', 100)
-            ->render();
+        $this->button->setAttribute('data-amount', 100);
 
-        $this->assertHasAttribute($button, 'data-amount', 100);
+        $this->assertButtonHasAttribute('data-amount', 100);
     }
 
     /**
-     * Assert that HTML has an attribute.
-     *
-     * @param  string $haystack
      * @param  string $name
      * @param  mixed  $value
      *
      * @return void
      */
-    protected function assertHasAttribute($haystack, $name, $value = null)
+    protected function assertButtonHasAttribute($name, $value = null)
     {
         if (is_null($value)) {
-            return $this->assertContains($name, $haystack);
+            return $this->assertContains(
+                $name,
+                $this->button->render()
+            );
         }
 
-        return $this->assertContains(sprintf('%s="%s"', $name, $value), $haystack);
+        return $this->assertContains(
+            sprintf('%s="%s"', $name, $value),
+            $this->button->render()
+        );
     }
 }
